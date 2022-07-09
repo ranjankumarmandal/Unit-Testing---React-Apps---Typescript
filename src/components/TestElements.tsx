a89f4c3a12d3dae4cc6d0f2d45dfa8c810026b6a
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-09 22:31:07 +0530

    created a button element inside jsx part of component TestElements along with disabled, onClick event and data-testid

--- src/components/TestElements.tsx
+++ src/components/TestElements.tsx
@@ -9,6 +9,13 @@ const TestElements = () => {
       <button data-testid='button-up' onClick={() => setCounter(counter + 1)}>
         Up
       </button>
+      <button
+        disabled
+        data-testid='button-down'
+        onClick={() => setCounter(counter - 1)}
+      >
+        Down
+      </button>
     </>
   );
 };
