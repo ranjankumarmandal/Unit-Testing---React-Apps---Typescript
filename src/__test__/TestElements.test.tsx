ee00f5d08449d47b52d5729fefe13dc7cc091097
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-09 23:16:29 +0530

    started working for test 1 : Test if the counter is equal to 0:

--- src/__test__/TestElements.test.tsx
+++ src/__test__/TestElements.test.tsx
@@ -5,3 +5,5 @@ import { render, cleanup } from '@testing-library/react';
 import TestElements from '../components/TestElements';
 
 afterEach(cleanup);
+
+// testt 1 : Test if the counter is equal to 0:
