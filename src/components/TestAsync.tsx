cfc1d4b2f24a269cfe08f77d06d2a00330e60389
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-10 22:37:47 +0530

    completed the creation of component TestAsync.tsx

--- src/components/TestAsync.tsx
+++ src/components/TestAsync.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TestAsync = () => {
-  return <div>TestAsync</div>;
+  const [counter, setCounter] = useState(0);
+
+  const handleAsyncClick = () => {
+    setTimeout(() => {
+      setCounter(counter + 1);
+    }, 5000);
+  };
+
+  return (
+    <>
+      <h1>{counter}</h1>
+      <button data-testid='button-up' onClick={handleAsyncClick}>
+        Up
+      </button>
+      <button data-testid='button-down' onClick={() => setCounter(counter - 1)}>
+        Down
+      </button>
+    </>
+  );
 };
 
 export default TestAsync;
