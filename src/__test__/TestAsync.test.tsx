88c90d40856d65e9c77f8d528521da061759b766
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-10 23:20:01 +0530

    completed tests for TestAsync.test.tsx

--- src/__test__/TestAsync.test.tsx
+++ src/__test__/TestAsync.test.tsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import { cleanup, render, fireEvent, waitFor } from '@testing-library/react';
+import TestAsync from '../components/TestAsync';
+
+afterEach(cleanup);
+
+it('increment counter should be after 5 second for button-up', async () => {
+  const { getByTestId, getByText, findByTestId } = render(<TestAsync />);
+  fireEvent.click(getByTestId('button-up'));
+
+  const counter = await findByTestId('counter');
+
+  expect(counter).toHaveTextContent('1');
+});
