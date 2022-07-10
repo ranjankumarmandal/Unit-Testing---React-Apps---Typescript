be5c659e692e5052967f71a3e7227f29042623a1
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-10 18:29:21 +0530

    created two buttons for increament and decreament event having attribute data-testid

--- src/components/TestEvent.tsx
+++ src/components/TestEvent.tsx
@@ -6,6 +6,12 @@ const TestEvent = () => {
   return (
     <>
       <h1 data-testid='counter'>Counter</h1>
+      <button data-testid='button-up' onClick={() => setCounter(counter + 1)}>
+        Up
+      </button>
+      <button data-testid='button-down' onClick={() => setCounter(counter - 1)}>
+        Down
+      </button>
     </>
   );
 };
