181fa0a56f338f1bf7142232f82a6b048cdf5691
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-10 18:24:32 +0530

    created h1 in jsx part of component TestEvent with attribute data-testid which will be used later to target during unit testing with jest

--- src/components/TestEvent.tsx
+++ src/components/TestEvent.tsx
@@ -3,7 +3,11 @@ import React, { useState } from 'react';
 const TestEvent = () => {
   const [counter, setCounter] = useState(0);
 
-  return <div>TestEvent</div>;
+  return (
+    <>
+      <h1 data-testid='counter'>Counter</h1>
+    </>
+  );
 };
 
 export default TestEvent;
