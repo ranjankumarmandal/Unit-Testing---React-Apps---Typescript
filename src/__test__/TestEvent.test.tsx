04392da37606b640d471faa1d39fd5e85798068b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-07-10 18:57:40 +0530

    added description for unit testing of inccreament counter event

--- src/__test__/TestEvent.test.tsx
+++ src/__test__/TestEvent.test.tsx
@@ -7,3 +7,6 @@ import TestEvent from '../components/TestEvent';
 afterEach(cleanup);
 
 // Test 1 : Test if increament counter event is working good
+it('increament counter event should work', () => {
+  // write tests here
+});
